80adb2c46e084ad7ab86ce470ff203b05edc23ff
D D <derksendhk@gmail.com>
2022-03-07 13:02:36 +0100

    product  pagina toegevoegd

---
+++ public/productKinderfiets.php
@@ -0,0 +1,7 @@
+<php 
+include header.php
+?>
+
+<h1>Dames fietsen</h1>
+<img src="../images/kinderfiets2.jpg" height="200" width="200">
+<img src="../images/kinderfiets1.jpg" height="200" width="200">
