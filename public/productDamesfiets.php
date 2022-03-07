d957fe1f02e7ce3dcd3c118cb7bf1138dd47ec71
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-03-07 13:35:33 +0100

    verbetering

--- public/productDamesfiets.php
+++ public/productDamesfiets.php
@@ -1,7 +1,11 @@
-<php 
+<?php
 include 'header.php';
 ?>
 
 <h1>Dames fietsen</h1>
 <img src="../images/damesfiets2.png" height="200" width="200">
 <img src="../images/damesfiets1.jpg" height="200" width="200">
+
+<?php
+include 'footer.php';
+?>
--- public/productKinderfiets.php
+++ public/productKinderfiets.php
@@ -1,7 +1,11 @@
-<php 
+<?php
 include 'header.php';
 ?>
 
 <h1>Dames fietsen</h1>
 <img src="../images/kinderfiets2.jpg" height="200" width="200">
 <img src="../images/kinderfiets1.jpg" height="200" width="200">
+
+<?php
+include 'footer.php';
+?>
