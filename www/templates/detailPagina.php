67ec54c344ee6f2fec35982ca92d1cc7a08753d9
[Sean Rosendaal] <[sean.rosendaal@student.graafschapcollege.nl]>
2022-03-21 14:56:47 +0100

    New detailPagina added + basic html

---
+++ www/templates/detailPagina.php
@@ -0,0 +1,16 @@
+<?php
+include 'Header.php'
+?>
+
+<div class="container">
+    <h1>Detailpagina</h1>
+    <br>
+    <h2></h2>
+    <img src="">
+    <p></p>
+</div>
+
+
+<?php
+include 'Footer.php'
+?>
