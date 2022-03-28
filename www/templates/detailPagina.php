81c2cfa8c449445ac58010e27297b9cbf2da0381
[Sean Rosendaal] <[sean.rosendaal@student.graafschapcollege.nl]>
2022-03-28 13:10:47 +0200

    detailPagina updated

--- www/templates/detailPagina.php
+++ www/templates/detailPagina.php
@@ -1,15 +1,8 @@
 <?php
 include 'Header.php'
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>My great web site</title>
-    <meta name="description" contents="Just a test website for learning html, css and php">
-    <link rel="stylesheet" href="../css/style.css" type="text/css">
-</head>
-<body>
+<!-- -->
+
     <div class="container">
         <h1>Detailpagina</h1>
         <br>
@@ -17,8 +10,6 @@ include 'Header.php'
         <img src="">
         <p></p>
     </div>
-</body>
-</html>
 
 <?php
 include 'Footer.php'
