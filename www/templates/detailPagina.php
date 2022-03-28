4944bdd33be963886a42a5a47b771fc97d3fbf46
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-03-28 13:07:34 +0200

    Update detailPagina.php

--- www/templates/detailPagina.php
+++ www/templates/detailPagina.php
@@ -2,6 +2,14 @@
 include 'Header.php'
 ?>
 
+    <div class="container">
+        <h1>Detailpagina</h1>
+        <br>
+        <h2></h2>
+        <img src="">
+        <p></p>
+    </div>
+
 
 
 <?php
