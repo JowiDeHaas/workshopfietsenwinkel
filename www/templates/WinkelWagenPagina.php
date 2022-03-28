bccbba4fc8a30e692b59095e5176ecb7992fa192
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-03-28 14:34:27 +0200

    winkelwagen voor nu af

--- www/templates/WinkelWagenPagina.php
+++ www/templates/WinkelWagenPagina.php
@@ -3,7 +3,22 @@ include 'Header.php';
 ?>
 <div class="container">
     <div>
-        
+        <table>
+            <?php
+            $placehold = '<tr>
+                <td><img src=""></td>
+                <td><b>Fiets</b></td>
+                <td>Op voorraad</td>
+                <td>aantal <input type="number" value="1"></td>
+                <td>69.42</td>
+            </tr>';
+            for($i = 0; $i < 6; $i++)
+            {
+                echo $placehold;
+            }
+            ?>
+        </table>
+        <button>Bestellen</button>
     </div>
 </div>
 <?php
