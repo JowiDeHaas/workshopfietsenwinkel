86702f1f0b3c759afb9edfed846a34fa9b9ba1ee
[Sean Rosendaal] <[sean.rosendaal@student.graafschapcollege.nl]>
2022-03-28 12:31:39 +0200

    detailPagina now has a link to the css

--- www/templates/detailPagina.php
+++ www/templates/detailPagina.php
@@ -1,15 +1,24 @@
 <?php
 include 'Header.php'
 ?>
-
-<div class="container">
-    <h1>Detailpagina</h1>
-    <br>
-    <h2></h2>
-    <img src="">
-    <p></p>
-</div>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My great web site</title>
+    <meta name="description" contents="Just a test website for learning html, css and php">
+    <link rel="stylesheet" href="../css/style.css" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <h1>Detailpagina</h1>
+        <br>
+        <h2></h2>
+        <img src="">
+        <p></p>
+    </div>
+</body>
+</html>
 
 <?php
 include 'Footer.php'
