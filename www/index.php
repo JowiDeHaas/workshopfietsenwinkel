46220196fa6883b09a9c117d2c61e40f23b6dcfc
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-04-04 13:59:06 +0200

    index

--- www/index.php
+++ www/index.php
@@ -6,9 +6,31 @@ $content = "Content";
 include $header;
 ?>
 <div class="container">
-    <br>
-    <?php echo $content ?>
-    <br>
+    
+    <!-- <?php echo $content ?> -->
+    <h1>aanbevolen fietsen</h1>
+    <div class=aanbevolen>
+        
+            <?php
+                for($i=0;$i<4;$i++)
+                {
+                    echo '<img src="img/herenfiets2.jpg" height="200px">';
+                }
+            ?>
+                   
+    </div>
+    <div height="200px"></div>
+    <h1>Fietsen met korting</h1>
+    <div class=aanbevolen>
+        
+            <?php
+                for($i=0;$i<4;$i++)
+                {
+                    echo '<img src="img/herenfiets2.jpg" height="200px">';
+                }
+            ?>
+                   
+    </div>
 </div>
 <?php
 include $footer;
