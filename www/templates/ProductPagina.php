839ccf6d063f635c85bf1798af614a1d31eb1b1d
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-04-04 12:55:44 +0200

    Update ProductPagina.php

--- www/templates/ProductPagina.php
+++ www/templates/ProductPagina.php
@@ -3,16 +3,16 @@ include 'Header.php';
 ?>
 <div class="container">
     <h1>Heren fietsen</h1>
-    <img src="../img/herenfiets2.jpg" height="200" width="200">
-    <img src="../img/herenfiets1.jpg" height="200" width="200">
+    <a href="detailPagina.php"><img src="../img/herenfiets2.jpg" height="200" width="200"></a>
+    <a href="detailPagina.php"><img src="../img/herenfiets1.jpg" height="200" width="200"></a>
 
     <h1>Kinderfietsen</h1>
-    <img src="../img/kinderfiets2.jpg" height="200" width="200">
-    <img src="../img/kinderfiets1.jpg" height="200" width="200">
+    <a href="detailPagina.php"><img src="../img/kinderfiets2.jpg" height="200" width="200"></a>
+    <a href="detailPagina.php"><img src="../img/kinderfiets1.jpg" height="200" width="200"></a>
 
     <h1>Damesfietsen</h1>
-    <img src="../img/damesfiets2.png" height="200" width="200">
-    <img src="../img/damesfiets1.jpg" height="200" width="200">
+    <a href="detailPagina.php"><img src="../img/damesfiets2.png" height="200" width="200"></a>
+    <a href="detailPagina.php"><img src="../img/damesfiets1.jpg" height="200" width="200"></a>
 </div>
 
 <?php
