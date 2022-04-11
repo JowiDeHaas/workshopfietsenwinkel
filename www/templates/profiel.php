989e730d0d530003ba7f77a9659980a01e724231
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-04-11 15:14:59 +0200

    data

--- www/templates/profiel.php
+++ www/templates/profiel.php
@@ -2,18 +2,20 @@
 include 'Header.php';
 ?>
 <?php
-$data = db_getData("SELECT * FROM fietsen WHERE 1");
+$data = db_getData("SELECT * FROM users WHERE 1");
 if ($data->num_rows > 0) {
     while($row = $data->fetch_assoc()) {
 //                    printf("Id: %s, Title: %s, Author: %s, Date: %d <br />",
         $id = $row["id"];
-        $merk = $row["merk"];
-        $kleur = $row["kleur"];
-        $model = $row["model"];
-        $versnellingen = $row["versnellingen"];
-        $elektrisch = $row["elektrisch"];
-        $remtype = $row["remtype"];
-        $prijs = $row["prijs"];
+        $voorn = $row["voornaam"];
+        $achtern = $row["achternaam"];
+        $ww = $row["wachtwoord"];
+        $mail = $row["email"];
+        $hnummer = $row["huisnummer"];
+        $straatn = $row["straatnaam"];
+        $stadn = $row["stadnaam"];
+        $land = $row["land"];
+        $postcode = $row["postcode"];
     }
 } else {
     printf('No record found.<br />');
@@ -22,10 +24,10 @@ if ($data->num_rows > 0) {
 <div class="container">
     <a href="index.php"><button>Uitloggen</button></a><br>
     <img src="../img/Legeprofielfoto.jpg" height="150px" width="150px">
-    <h1>Jan Hansen</h1>
-    <h3>Wachtwoord: ****************</h3>
-    <h3>voorbeeld@gmail.com</h3>
-    <h3>Adres: voorbeeld adres</h3>
+    <h1><?php echo $voorn . " " . $achtern?></h1>
+    <h3>Wachtwoord: <?php echo $ww ?></h3>
+    <h3><?php echo $mail?></h3>
+    <h3>Adres: <?php echo $hnummer . " " . $straatn . ", " . $stadn . ", ". $land . ", " . $postcode  ?></h3>
     <h2>Bestelgeschiedenis</h2>
     <table id="geschiedenis">
         <tr>
